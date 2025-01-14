f2605124c53ea275255b9e304240e508ec72276b
Jacob Lopez <jacoblopezfinancial@outlook.com>
2025-01-14 15:27:37 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Calculator
+{
+   public static int number1 = 5;
+   public static int number2 = 10;
+
+   public static int Add(int a, int b)
+   {
+       return a + b;
+   }
+
+   public static void Main(string[] args)
+   {
+       int result = Add(number1, number2);
+       Console.WriteLine("Sum: " + result);
+   }
+}
